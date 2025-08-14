bde041de4765dce5d4c960ca8ed444e4b21bdc2f
tarc071 <tarcisiomota@live.com>
2025-08-14 23:25:28 +0000

    NOVATO

--- hipertrunfo.c
+++ hipertrunfo.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    printf("OLA, MUNDO, OLA!\n");
+    printf("BEM VINDOS AO HIPER TRUNFO, HDSPM!\n");
+
+    char estado1[20], estado2[20], cod1[20], cod2[20], cidade1[20], cidade2[20]; 
+    int populacao1, populacao2, turismo1, turismo2;
+    float area1, area2, pib1, pib2;
+
+    printf("INSIRA DADOS DO ESTADO 01\n");
+    
+
+
+
 
     return 0;
 }
