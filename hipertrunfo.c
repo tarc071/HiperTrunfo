13e7c017d5f52ac5f5ac9780836fe59661d9ce1e
tarc071 <tarcisiomota@live.com>
2025-08-14 23:30:49 +0000

    desnovato

--- hipertrunfo.c
+++ hipertrunfo.c
@@ -8,7 +8,33 @@ int main()
     int populacao1, populacao2, turismo1, turismo2;
     float area1, area2, pib1, pib2;
 
-    printf("INSIRA DADOS DO ESTADO 01\n");
+    printf("INSIRA DADOS DA CARTA 01\n");
+
+    printf("INSIRA NOME DO ESTADO 01\n");
+    scanf("%s", estado1);
+    printf("INSIRA CÓDIGO DA CARTA 01\n");
+    scanf("%s", &cod1);
+    printf("INSIRA CIDADE DO ESTADO 01\n");
+    scanf("%s", &cidade1);
+    printf("INSIRA POPULACAO DO ESTADO 01\n");
+    scanf("%d", &populacao1);
+    printf("INSIRA ÁREA DO ESTADO 01\n");
+    scanf("%f", &area1);
+    printf("INSIRA PIB DO ESTADO 01\n");
+    scanf("%f", pib1);
+    printf("INSIRA PONTOS TURISTICOS DO ESTADO 01\n");
+    scanf("%d", &turismo1);
+
+    printf("AGORA VAMOS AOS RESULTADOS, CABRONES!\n");
+
+    printf("ESTADO 01: %s\n", estado1);
+    printf("CÓDIGO 01: %S", cod1);
+    printf("CIDADE 01: %S", cidade1);
+    printf("POPULACAO 01: %d", populacao1);
+    printf("AREA 01: %f", area1);
+    printf("PIB 01: %f", pib1);
+    printf("PONTOS TURISTICOS 01: %d", turismo1);
+
     
 
 
